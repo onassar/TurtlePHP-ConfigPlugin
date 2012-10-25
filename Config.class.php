c084f167ae65f4a77d8cf00624635a93f90df24d
Oliver Nassar <onassar@gmail.com>
2012-10-25 13:36:31 -0400

    Doc tweak

--- Config.class.php
+++ Config.class.php
@@ -10,8 +10,8 @@
      * application configuration. While not comprehensive, clean and should be
      * used as a standard for TurtlePHP applications.
      *
-     * @author   Oliver Nassar <onassar@gmail.com>
      * @abstract
+     * @author   Oliver Nassar <onassar@gmail.com>
      */
     abstract class Config
     {
